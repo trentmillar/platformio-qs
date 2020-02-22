5aa6775f0ea4326f05b8381546c65790586cb8e1
trentmillar <trent.millar@gmail.com>
2020-02-22 13:25:17 -0700

    including local painless

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,17 @@ extern "C" {
 	#include "freertos/timers.h"
 }
 #include <AsyncMqttClient.h>
-#include "esp_event_legacy.h"
-#include "WiFiType.h"
+
+#include "painlessMesh.h"
+
 
 #define WIFI_SSID "yourSSID"
 #define WIFI_PASSWORD "yourpass"
 
 #define MQTT_HOST IPAddress(192, 168, 1, 10)
 #define MQTT_PORT 1883
+
+
 
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
