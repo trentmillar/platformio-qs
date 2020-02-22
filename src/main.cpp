545c30c161f7d49f88700e85d09a541816b06122
trentmillar <trent.millar@gmail.com>
2020-02-21 22:02:19 -0700

    back out painlessMess

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
  */
 #include <SPI.h>
 #include <SPIFFS.h>
-#include <Update.h>
 #include "Arduino.h"
 
 #ifndef LED_BUILTIN
@@ -15,21 +14,8 @@
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
 }
